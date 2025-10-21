d8a5fc4ac80732ded68463547176f41c3c965de1
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-21 16:17:31 -0300

    Criando Repository's

---
+++ ArchitectureClean.Domain/Interfaces/IAdministradorRepository.cs
@@ -0,0 +1,18 @@
+﻿using ArchitectureClean.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitectureClean.Domain.Interfaces
+{
+    public interface IAdministradorRepository
+    {
+        Task<Administrador?> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Administrador>> ListarAsync();
+        Task AdicionarAsync(Administrador administrador);
+        Task AtualizarAsync(Administrador administrador);
+        Task RemoverAsync(Administrador administrador);
+    }
+}
---
+++ ArchitectureClean.Domain/Interfaces/IEstagiarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using ArchitectureClean.Domain.Entities;
+
+namespace ArchitectureClean.Domain.Interfaces
+{
+    public interface IEstagiarioRepository
+    {
+        Task<Estagiario?> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Estagiario>> ListarAsync();
+        Task AdicionarAsync(Estagiario estagiario);
+        Task AtualizarAsync(Estagiario estagiario);
+        Task RemoverAsync(Estagiario estagiario);
+    }
+}
