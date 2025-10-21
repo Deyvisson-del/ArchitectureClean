b743fe05c464ea412b847907957107d70f165bf7
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-21 15:55:16 -0300

    Criação dos DTOs

---
+++ ArchitectureClean.Application/DTOs/AdministradorDTO.cs
@@ -0,0 +1,18 @@
+﻿using ArchitectureClean.Domain.Enuns;
+using ArchitectureClean.Domain.ValueObject;
+
+namespace ArchitectureClean.Application.DTOs
+{
+    public class AdministradorDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; private set; } = string.Empty;
+
+        public Email Email { get; private set; } = default!;
+
+        public Senha Senha { get; private set; } = default!;
+
+        public Perfil Perfil { get; private set; } = Perfil.ADM;
+    }
+}
---
+++ ArchitectureClean.Application/DTOs/EstagiarioDTO.cs
@@ -0,0 +1,21 @@
+using ArchitectureClean.Domain.Enuns;
+using ArchitectureClean.Domain.ValueObject;
+
+namespace ArchitectureClean.Application.DTOs
+{
+    public class EstagiarioDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; private set; } = string.Empty;
+
+        public Email Email { get; private set; } = default!;
+
+        public Senha Senha { get; private set; } = default!;
+
+        public Perfil Perfil { get; private set; } = Perfil.EST;
+
+        public Status Status { get; private set; }
+
+    }
+}
