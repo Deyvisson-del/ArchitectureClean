0820db3f4042b73b1c8ea1acb468475542ecefd2
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-21 16:26:16 -0300

    Criação dos primeiro caso de uso

---
+++ ArchitectureClean.Application/UseCases/CadastrarEstagiarioUseCase.cs
@@ -0,0 +1,28 @@
+﻿using ArchitectureClean.Domain.Entities;
+using ArchitectureClean.Domain.Enuns;
+using ArchitectureClean.Domain.Interfaces;
+using ArchitectureClean.Domain.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitectureClean.Application.UseCases
+{
+    public class CadastrarEstagiarioUseCase
+    {
+        private readonly IEstagiarioRepository _estagiarioRepository;
+
+        public CadastrarEstagiarioUseCase(IEstagiarioRepository estagiarioRepository)
+        {
+            _estagiarioRepository = estagiarioRepository;
+        }
+
+        public async Task Execute(string nome, string email, string senha)
+        {
+            var estagiario = new Estagiario(nome, new Email(email), new Senha(senha), Perfil.EST, Status.AT);
+            await _estagiarioRepository.AdicionarAsync(estagiario);
+        }
+    }
+}
