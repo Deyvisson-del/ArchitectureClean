f5ffb347e7115651dfc17b5851ee9c61934b922a
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-22 10:30:29 -0300

    Correção da camada application, Interfaces Repository

---
+++ ArchitectureClean.Application/Interfaces/IAdministradorRepository.cs
@@ -0,0 +1,13 @@
+﻿using ArchitectureClean.Domain.Entities;
+
+namespace ArchitectureClean.Application.Interfaces
+{
+    public interface IAdministradorRepository
+    {
+        Task<Administrador?> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Administrador>> ListarAsync();
+        Task AdicionarAsync(Administrador administrador);
+        Task AtualizarAsync(Administrador administrador);
+        Task RemoverAsync(Administrador administrador);
+    }
+}
---
+++ ArchitectureClean.Application/Interfaces/IEstagiarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using ArchitectureClean.Domain.Entities;
+
+namespace ArchitectureClean.Application.Interfaces
+{
+    public interface IEstagiarioRepository
+    {
+        Task<Estagiario?> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Estagiario>> ListarAsync();
+        Task AdicionarAsync(Estagiario estagiario);
+        Task AtualizarAsync(Estagiario estagiario);
+        Task RemoverAsync(Estagiario estagiario);
+    }
+}
