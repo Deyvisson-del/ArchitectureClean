7a9fd40d42a26335af115537e7cca42ef3010ec2
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-22 14:51:20 -0300

    Criação da Controller Administrador

--- ArchitectureClean.MVC/Controllers/AdministradorController.cs
+++ ArchitectureClean.MVC/Controllers/AdministradorController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArchitectureClean.Infra.IoC.Persistence;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArchitectureClean.MVC.Controllers
 {
     public class AdministradorController : Controller
     {
 
+        private readonly AppDbContext _context;
 
+        public AdministradorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var administradores = _context.Administradores.ToList();
+            return View(administradores);
+        }
     }
 }
