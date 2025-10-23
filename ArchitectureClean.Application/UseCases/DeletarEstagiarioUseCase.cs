6beef3811587ea374a85d9043848436d650d253f
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-23 15:15:39 -0300

    Criação dos Casos de Uso dos Estagiarios

--- ArchitectureClean.Application/UseCases/DeletarEstagiarioUseCase.cs
+++ ArchitectureClean.Application/UseCases/DeletarEstagiarioUseCase.cs
@@ -1,6 +1,20 @@
-﻿namespace ArchitectureClean.Application.UseCases
+﻿using ArchitectureClean.Domain.Interfaces;
+
+namespace ArchitectureClean.Application.UseCases
 {
     public class DeletarEstagiarioUseCase
     {
+
+        private readonly IEstagiarioRepository _estagiarioRepository;
+        public DeletarEstagiarioUseCase(IEstagiarioRepository estagiarioRepository)
+        {
+            _estagiarioRepository = estagiarioRepository;
+        }
+        public async Task ExecuteAsync(Guid id)
+        {
+            var estagiario = await _estagiarioRepository.ObterPorIdAsync(id);
+            if (estagiario == null) throw new Exception("Estagiário não encontrado.");
+            await _estagiarioRepository.RemoverAsync(estagiario);
+        }
     }
 }
