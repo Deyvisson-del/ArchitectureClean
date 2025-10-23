135eba7cc48995abd498c794f1ab227ad881471d
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-23 15:29:52 -0300

    Controller Estagiario

--- ArchitectureClean.MVC/Controllers/EstagiarioController.cs
+++ ArchitectureClean.MVC/Controllers/EstagiarioController.cs
@@ -9,12 +9,50 @@ namespace ArchitectureClean.MVC.Controllers
     {
 
         private readonly CadastrarEstagiarioUseCase _cadastrarEstagiarioUseCase;
+        private readonly BuscarEstagiarioPorIdUseCase _buscarEstagiarioPorIdUseCase;
+        private readonly AtualizarEstagiarioUseCase _atualizarEstagiariosUseCase;
+        private readonly DeletarEstagiarioUseCase _deletarEstagiarioUseCase;
 
-        public EstagiarioController(CadastrarEstagiarioUseCase cadastrarEstagiario)
+
+        public EstagiarioController(CadastrarEstagiarioUseCase cadastrarEstagiario, BuscarEstagiarioPorIdUseCase buscarEstagiarioPorIdUseCase, AtualizarEstagiarioUseCase atualizarEstagiariosUseCase, DeletarEstagiarioUseCase deletarEstagiarioUseCase)
         {
             _cadastrarEstagiarioUseCase = cadastrarEstagiario;
+            _buscarEstagiarioPorIdUseCase = buscarEstagiarioPorIdUseCase;
+            _atualizarEstagiariosUseCase = atualizarEstagiariosUseCase;
+            _deletarEstagiarioUseCase = deletarEstagiarioUseCase;
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> BuscarEstagiarioPorId(Guid id)
+        {
+            try
+            {
+                var estagiario = await _buscarEstagiarioPorIdUseCase.ExecuteAsync(id);
+                return View(estagiario);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View();
+            }
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletarEstagiario(Guid id)
+        {
+            try
+            {
+                await _deletarEstagiarioUseCase.ExecuteAsync(id);
+                return Ok("Estagiário deletado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View();
+            }
+        }
 
         [HttpPost]
         public async Task<IActionResult> CadastrarEstagiario([FromBody] EstagiarioDTO estagiarioDTO)
