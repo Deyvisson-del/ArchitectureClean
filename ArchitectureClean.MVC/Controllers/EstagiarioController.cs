0c1a69e0b01462c0883fea615c845d592cc5f473
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-23 11:28:21 -0300

    Criação do controller Estagiario

--- ArchitectureClean.MVC/Controllers/EstagiarioController.cs
+++ ArchitectureClean.MVC/Controllers/EstagiarioController.cs
@@ -1,20 +1,34 @@
-﻿using ArchitectureClean.Infra.IoC.Persistence;
+﻿using ArchitectureClean.Application.DTOs;
+using ArchitectureClean.Application.UseCases;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace ArchitectureClean.MVC.Controllers
 {
     public class EstagiarioController : Controller
     {
-        private readonly AppDbContext _context;
 
-        public EstagiarioController(AppDbContext context)
+        private readonly CadastrarEstagiarioUseCase _cadastrarEstagiarioUseCase;
+
+        public EstagiarioController(CadastrarEstagiarioUseCase cadastrarEstagiario)
         {
-            _context = context;
+            _cadastrarEstagiarioUseCase = cadastrarEstagiario;
         }
-        public IActionResult Index()
+
+
+        [HttpPost]
+        public async Task<IActionResult> CadastrarEstagiario([FromBody] EstagiarioDTO estagiarioDTO)
         {
-            var estagiarios = _context.Estagiarios.ToList();
-            return View(estagiarios);
+            try
+            {
+                await _cadastrarEstagiarioUseCase.ExecuteAsync(estagiarioDTO);
+                return Ok("Estagiário cadastrado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View(estagiarioDTO);
+            }
         }
     }
 }
