11e38a776ac6a473ff16e247912ce96b96d0acec
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-24 17:43:09 -0300

    Atualização da Migration

---
+++ ArchitectureClean.Infra.Data/Migrations/20251024203936_AddAdminSeed.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArchitectureClean.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Senha",
+                table: "Administradores");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Administradores",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(60)",
+                oldMaxLength: 60)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenhaHash",
+                table: "Administradores",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SenhaHash",
+                table: "Administradores");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Administradores",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Senha",
+                table: "Administradores",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
