95f085d193737f98caff70766c3d251b73aa26a5
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-10-30 12:28:58 -0300

    loginDTO

--- ArchitectureClean.Application/DTOs/LoginDTO.cs
+++ ArchitectureClean.Application/DTOs/LoginDTO.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ArchitectureClean.Domain.ValueObject;
 
 namespace ArchitectureClean.Application.DTOs
 {
     public class LoginDTO
     {
 
-
+        public Email email { get; set; } = default!;
+        public Senha senha { get; set; } = default!;
 
     }
 }
