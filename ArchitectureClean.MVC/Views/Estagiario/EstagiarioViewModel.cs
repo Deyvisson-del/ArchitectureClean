50f72e8083ef91da2651ebdbfab9169e84bb3674
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-03 15:49:47 -0300

    iniciando desenvolvimento de viewsModel e index.cshtml

--- ArchitectureClean.MVC/Views/Estagiario/EstagiarioViewModel.cs
+++ ArchitectureClean.MVC/Views/Estagiario/EstagiarioViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using ArchitectureClean.Domain.Enuns;
+using ArchitectureClean.Domain.Entities;
 using ArchitectureClean.Domain.ValueObject;
+using System.ComponentModel;
 
 namespace ArchitectureClean.MVC.Views.Estagiario
 {
-    public class EstagiarioViewModel
+    public class EstagiarioViewModel : INotifyPropertyChanged
     {
+        private Estagiario _estagiario;
+
         public string Nome { get; set; } = string.Empty;
         public Email? email { get; set; }
         public Senha? Senha { get; set; }
         public Perfil Perfil { get; private set; }
         public Status Status { get; private set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }   
 }
