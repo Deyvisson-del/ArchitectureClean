1e2f3ba6a2aed354c27274ef6540d2d5640884b8
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-03 00:01:29 -0300

    Iniciando Views Model

---
+++ ArchitectureClean.MVC/Views/EstagiarioViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ArchitectureClean.Domain.Enuns;
+using ArchitectureClean.Domain.ValueObject;
+
+namespace ArchitectureClean.MVC.Views
+{
+    public class EstagiarioViewModel
+    {
+        public string Nome { get; set; } = string.Empty;
+        public Email email { get; set; }
+        public Senha Senha { get; set; }
+        public Perfil Perfil { get; private set; }
+        public Status Status { get; private set; }
+    }
+}
